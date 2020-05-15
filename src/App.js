c6fc9a0575d49204d41fe246e76526b01e155b62
Ripudaman <ripudamansinghthind@gmail.com>
2020-05-15 13:31:14 -0700

    added basic design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import './components/css/menu.css';
+
+import SortingContainer from './components/Layout/SortingContainer';
 
 
 function App() {
   return (
-    <div id = "container-center" style = {{backgroundColor: "white"}}>
-    <h5>Hello!</h5>
+    <div id = "container" style = {{backgroundColor: "white"}}>
+        <Router>
+        <nav className="menu" id="menu">
+          <div className="menu-content">
+            <ul className="menu-items">
+                <li className="menu-item">
+                  <Link to="/Hide">
+                  <div className="clickyButton">Hide
+                  </div>
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="/">
+                  <div className="clickyButton">Make Array
+                  </div>
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="/">
+                  <div className="clickyButton">Binary Sort
+                  </div>
+                  </Link>
+                </li>
+                <li className="menu-item">
+                  <Link to="/">
+                  <div className="clickyButton">Settings
+                  </div>
+                  </Link>
+                </li>
+            </ul>
+          </div>
+      </nav>
+        <Switch>
+                <Route exact path="/">
+                <SortingContainer />
+                </Route>
+                <Route path="/Experience">
+                </Route>
+                <Route path="/Projects">
+                </Route>
+              </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Layout/SortingContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../css/sorting.css'
+
+const Profile = (props) => {
+  return (
+    <div className="card" id="Profile">
+      <div className="row-card">
+      </div>
+    </div>
+  );
+
+}
+
+export default Profile;
