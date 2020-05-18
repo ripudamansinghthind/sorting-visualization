2a0fe09d9a325cf4bc2b1debf16969de4dc7114c
Ripudaman <ripudamansinghthind@gmail.com>
2020-05-18 00:54:59 -0700

    sorting animation complete, thanks to Ian

--- src/components/Layout/BubbleSort.js
+++ src/components/Layout/BubbleSort.js
@@ -1,49 +1,52 @@
 import React from 'react';
 import '../css/sorting.css';
 
-export class BubbleSort extends React.Component {
-    
+/*this delay in state using promise idea is a huge thanks to https://ianmackay.io/ */
+const call = async (fn) => {
+  await new Promise(r => setImmediate(r));
+  return fn();
+}
+
+export default class BubbleSort extends React.Component {
+
   constructor(props) {
     super(props);
-    this.testVarible= this.props.message;
-
-    this.state = {
-      array: []
-  };
+    this.state = {array: this.props.message};
   }
 
   componentDidMount() {
-    this.haha(this.testVarible);
+    this.sortFunction(this.props.message)
   }
 
-  async haha(parameter) {
-    var n = parameter.length;
-    for(var i=0; i<n-1; i++) {
-      for(var j=0; j<n-i-1; j++) {
-        if(parameter[j] > parameter[j+1]) {
-          var temp = parameter[j];
-          parameter[j] = parameter[j+1];
-          parameter[j+1] = temp;
-        }  
-        this.setState({ array: parameter });
+  swap = (array, x, y) => {
+    [array[x], array[y]] = [array[y], array[x]]
+    this.setState({array: array})
+    return array;
+  }
+
+  async sortFunction(parameter) {
+    const n = parameter.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (parameter[j] > parameter[j + 1]) {
+          parameter = await call(() => this.swap(parameter, j, j + 1));
+        }
       }
     }
-}
+  }
 
 
   render() {
-    let {array} = this.state;
-  return (
+    const {array} = this.state;
+    return (
       <div className="array-container">
-      {array.map((value, idx) => (
-          <div 
-          className="array-bar" 
-          key={idx}
-          style={{height: `${value}px`}}></div>
-      ))}
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{height: `${value}px`}}></div>
+        ))}
       </div>
-       );
-    }
+    );
+  }
 }
-
-export default BubbleSort;
