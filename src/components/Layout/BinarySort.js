d643534d26eaa65f76e43da27be3ce5ced48f6dc
Ripudaman <ripudamansinghthind@gmail.com>
2020-05-17 19:28:38 -0700

    minor update to bubble sort

--- src/components/Layout/BinarySort.js
+++ src/components/Layout/BinarySort.js
@@ -8,7 +8,7 @@ export class BubbleSort extends React.Component {
     this.testVarible= this.props.message;
 
     this.state = {
-      array: [],
+      array: []
   };
   }
 
@@ -16,51 +16,34 @@ export class BubbleSort extends React.Component {
     this.haha(this.testVarible);
   }
 
-  haha(parameter) {
+  async haha(parameter) {
     var n = parameter.length;
     for(var i=0; i<n-1; i++) {
       for(var j=0; j<n-i-1; j++) {
         if(parameter[j] > parameter[j+1]) {
           var temp = parameter[j];
-          this.setState({ array: parameter });
           parameter[j] = parameter[j+1];
-          this.setState({ array: parameter });
           parameter[j+1] = temp;
-          this.setState({ array: parameter });
-        }
+        }  
+        this.setState({ array: parameter });
       }
     }
 }
 
 
   render() {
-    const {array} = this.state;
+    let {array} = this.state;
   return (
       <div className="array-container">
-      {/* {array.map((value, idx) => (
+      {array.map((value, idx) => (
           <div 
           className="array-bar" 
           key={idx}
           style={{height: `${value}px`}}></div>
-      ))} */}
-      <p>{this.state.array}</p>
+      ))}
       </div>
        );
     }
 }
-
-// function sortingBinaryMethod(parameter) {
-//   var n = parameter.length;
-//   for(var i=0; i<n-1; i++) {
-//     for(var j=0; j<n-i-1; j++) {
-//       if(parameter[j] > parameter[j+1]) {
-//         var temp = parameter[j];
-//         parameter[j] = parameter[j+1];
-//         parameter[j+1] = temp;
-//       }
-//     }
-//   }
-//   return parameter;
-// }
 
 export default BubbleSort;
